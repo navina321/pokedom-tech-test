239b9b58d51397289ddd53ea8b0ae5c49afaf857
navina321 <81630548+navina321@users.noreply.github.com>
2023-01-25 16:33:22 +0000

    added search input for filtering

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
 import pokemonArray from "./data/pokemon.js";
-
 const cardContainer = document.querySelector(".card-container");
+const input = document.querySelector("#search-bar")
 
 // function for card container HTML
 
-
 const makeCard = (pokemonObject) => {
-    
-    //const x = ${pokemonObject.name.charAt(0).toUpperCase()} + ${pokemonObject.name.slice}
-    const capitalPokemon = pokemonObject.name.charAt(0).toUpperCase() + pokemonObject.name.slice(1);
+  const capitalPokemon =
+    pokemonObject.name.charAt(0).toUpperCase() + pokemonObject.name.slice(1);
 
-    const cardHTML = 
-    `<div class = "card">
-        <img class = "card__image" src = ${pokemonObject.sprite} alt = "image of ${pokemonObject.name}">
+  const cardHTML = `<div class = "card">
+        <img class = "card__image" src = ${
+          pokemonObject.sprite
+        } alt = "image of ${pokemonObject.name}">
         <div class = "card__content">
             <h1 class = "card__heading">${capitalPokemon}</h1>
-            <h2 class = "card__text">${capitalPokemon} (#${pokemonObject.id}) is a ${pokemonObject.types.join(" & ")} type pokemon.</h2>
+            <h2 class = "card__text">${capitalPokemon} (#${
+    pokemonObject.id
+  }) is a ${pokemonObject.types.join(" & ")} type pokemon.</h2>
         </div>
     </div> `;
-    
-    return cardHTML;
-}
 
-const populatePage =  () => {
-    for (let i = 0; i < pokemonArray.length; i++) {
-        cardContainer.innerHTML += makeCard (pokemonArray[i]);
-    }
+  return cardHTML;
 };
 
-// const populatePage = (pokemonArr) => {
-//     cardContainer.innerHTML = pokemonArr.map(pokemon => makeCard(character)).join("")
+const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase()
+    const filteredPokemonArray = pokemonArray.filter(pokemon => {
+      if(pokemon.name.toLowerCase().includes(searchTerm)){
+        return true;
+      } else {
+        return false;
+      }
+    })
+  populatePage(filteredPokemonArray)
+  }
 
-// }
-
-
+const populatePage = (pokemonArr) => {
+    cardContainer.innerHTML = pokemonArr.map(pokemon => makeCard(pokemon)).join("")
+};
 
 populatePage(pokemonArray);
+
+input.addEventListener("input",handleSearch)
+
+// const populatePage = () => {
+//   for (let i = 0; i < pokemonArray.length; i++) {
+//     cardContainer.innerHTML += makeCard(pokemonArray[i]);
+//   }
+// };
+
+
+
+
+
+
+
